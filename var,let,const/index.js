31204bfaab210c05d4925fae1218ad0e5280a4d6
shazaib491 <shazaibrehman127@gmail.com>
2024-01-17 11:04:52 +0530

    commited

---
+++ var,let,const/index.js
@@ -0,0 +1,62 @@
+console.log(a);
+
+// // console.log(a)
+const  a=10;
+
+
+
+
+
+hello()
+
+function hello() {
+    console.log("Hello!");
+    var no=10;
+    console.log(no);
+}
+
+
+{
+    const  username="admin";
+}
+
+
+
+
+// console.log(username)
+
+
+
+for(const i=0; i<10; i++)
+{
+console.log(i)
+}
+
+// console.log(i)
+
+
+let email="admin@gmail.com";
+
+// let email="user@gmail.com"
+email="admin@gmail.com"
+
+console.log(email)
+
+
+const password="12345";
+console.log(password)
+// var username="john";
+// username="admin";
+
+
+// console.log(username)
+
+// print(i);
+
+
+
+// // console.log(i);
+
+// console.log(name)
+
+
